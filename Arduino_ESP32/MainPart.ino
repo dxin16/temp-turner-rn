13f56be8a2a476f4b3d131d791c48ab8c0427ff6
Chyi1214 <89947346+Chyi1214@users.noreply.github.com>
2023-04-10 18:39:22 -0400

    With Gliding window

--- Arduino_ESP32/MainPart.ino
+++ Arduino_ESP32/MainPart.ino
@@ -1,16 +1,17 @@
+#include <Arduino.h>
 #include <WiFi.h>
 #include <WebServer.h>
 
 // Replace with your network credentials
-const char* ssid = "REPLACE_THIS";
-const char* password = "REPLACE_THIS";
+const char* ssid = "iPhone Chyi";
+const char* password = "hijason123";
 
 // Create an instance of the web server
 WebServer server(80);
 
 // Analog pins for temperature and smoke sensors
-const int tempPin = 34;
-const int smokePin = 35;
+const int tempPin = 35;
+const int smokePin = 34;
 
 // Current temperature and smoke level
 int currentTemp = 0;
@@ -65,9 +66,30 @@ void setup() {
   pinMode(smokePin, INPUT);
 }
 
+#define SAMPLE_SIZE 1000
+
+uint16_t temp_record[SAMPLE_SIZE];
+uint16_t temp_index = 0;
+int over_Sample(int currentTemp){
+  temp_record[temp_index] = currentTemp;
+  temp_index++;
+  if (temp_index > SAMPLE_SIZE) temp_index = 0;
+  
+  int sum = 0;
+  for(int i = 0; i < sizeof(temp_record) / sizeof(temp_record[0]); i++){
+    
+    sum += temp_record[i];
+    
+  }
+  return sum / SAMPLE_SIZE;
+}
+
+
 void loop() {
   // Read analog sensors
   currentTemp = analogRead(tempPin);
+  currentTemp = over_Sample(currentTemp);
+
   currentSmoke = analogRead(smokePin);
 
   // Update web page
